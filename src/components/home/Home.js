d63b9b261b81748420c073ca3f43be270f2a74e8
Sarag Mohan P <sarag.mohan@gmail.com>
2022-02-24 21:27:36 +0530

    Update Home.js
    Home page updated with all the information required

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,6 +6,7 @@ import Pic02 from '../images/pic02.jpg';
 import Pic03 from '../images/pic03.jpg';
 
 
+
 function Home(props) {
     return (
         <div>
@@ -31,10 +32,11 @@ function Home(props) {
                 <section id="one" className="wrapper style1 special">
                     <div className="inner">
                         <header className="major">
-                            <h2>Only 2% of resumes make it past the first round.<br />
-                                Be in the top 2%</h2>
-                            <p>Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet eleifend<br />
-                                fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus ullamcorper.</p>
+                            <h2>WE CAN HELP YOUR CAREER GROW<br />
+                                Get your Resume in 3 easy steps</h2>
+                            <p>Who have you always dreamt of becoming? We bet a resume writer didn’t make it to that list.<br />
+                                So don’t let a piece of paper turn your dreams into nightmares.With Sleekcvs, we<br />
+                                take care of the details, so you can focus on what really matters: your career..</p>
                         </header>
                         <ul className="icons major">
                             <li><span className="icon fa-gem major style1"><span className="label">Lorem</span></span></li>
@@ -49,25 +51,23 @@ function Home(props) {
                     <section className="spotlight">
                         <div className="image"><img src={Pic01} alt="" /></div>
                         <div className="content">
-                            <h2>Magna primis lobortis<br />
-                                sed ullamcorper</h2>
-                            <p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
+                            <h2>Pick a template<br />
+                                of your choice</h2>
+                            <p>Our professional resume templates are designed strictly following all industry guidelines and best practices employers are looking for.</p>
                         </div>
                     </section>
                     <section className="spotlight">
                         <div className="image"><img src={Pic02} alt="" /></div>
                         <div className="content">
-                            <h2>Tortor dolore feugiat<br />
-                                elementum magna</h2>
-                            <p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
+                            <h2>Fill in the blanks</h2>
+                            <p>Type in basic information. Use suggestions from recruitment pros for everything else.</p>
                         </div>
                     </section>
                     <section className="spotlight">
                         <div className="image"><img src={Pic03} alt="" /></div>
                         <div className="content">
-                            <h2>Augue eleifend aliquet<br />
-                                sed condimentum</h2>
-                            <p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
+                            <h2>Download your resume</h2>
+                            <p>Start impressing employers. Download your awesome resume and land the job you are looking for, effortlessly.</p>
                         </div>
                     </section>
                 </section>
@@ -76,34 +76,34 @@ function Home(props) {
                 <section id="three" className="wrapper style3 special">
                     <div className="inner">
                         <header className="major">
-                            <h2>Accumsan mus tortor nunc aliquet</h2>
-                            <p>Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet eleifend<br />
-                                fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus ullamcorper.</p>
+                            <h2>Benefits of using Sleekcvs</h2>
+                            <p>From avoiding common CV mistakes like inconsistencies or forgetting important data…<br />
+                                Here’s why you should consider using an online CV Builder!</p>
                         </header>
                         <ul className="features">
                             <li className="icon fa-paper-plane">
-                                <h3>Arcu accumsan</h3>
-                                <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
+                                <h3>Save hours of time</h3>
+                                <p>Our resume builder will save you time creating your resume. Simply choose a template, and our software will help you fill out each section and bullet point.</p>
                             </li>
                             <li className="icon solid fa-laptop">
-                                <h3>Ac Augue Eget</h3>
-                                <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
+                                <h3>Make matching application documents</h3>
+                                <p>Pair your resume template with a matching cover letter template to make your application more memorable and leave a stronger impression on employers.</p>
                             </li>
                             <li className="icon solid fa-code">
-                                <h3>Mus Scelerisque</h3>
-                                <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
+                                <h3>Build a resume with the power of AI</h3>
+                                <p>Our resume builder takes information from real job ads, then modifies it with AI to create unique bullet points for your work experience section.</p>
                             </li>
                             <li className="icon solid fa-headphones-alt">
-                                <h3>Mauris Imperdiet</h3>
-                                <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
+                                <h3>Enjoy a huge range of free templates</h3>
+                                <p>Resume Genius boasts the largest, most diverse selection of resume and cover letter templates on the internet. And they’re all free to download.</p>
                             </li>
                             <li className="icon fa-heart">
-                                <h3>Aenean Primis</h3>
-                                <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
+                                <h3>Create an ATS-friendly resume</h3>
+                                <p>All of our templates were designed to be fully compatible with the Applicant Tracking System (ATS) software companies use to filter candidates.</p>
                             </li>
                             <li className="icon fa-flag">
-                                <h3>Tortor Ut</h3>
-                                <p>Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem tincidunt nullam amet leo Aenean ligula consequat consequat.</p>
+                                <h3>More likely to land a job</h3>
+                                <p>With a representative and professional CV, you will stand out amongst all other applicants. You are also up to 65% more likely to be invited to an interview.</p>
                             </li>
                         </ul>
                     </div>
@@ -115,11 +115,10 @@ function Home(props) {
                         <li><Link to="#" class="icon brands fa-twitter"><span class="label">Twitter</span></Link></li>
                         <li><Link to="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></Link></li>
                         <li><Link to="#" class="icon brands fa-instagram"><span class="label">Instagram</span></Link></li>
-                        <li><Link to="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></Link></li>
                         <li><Link to="#" class="icon solid fa-envelope"><span class="label">Email</span></Link></li>
                     </ul>
                     <ul className="copyright">
-                        <li>&copy; Untitled</li><li>Design: <Link to="http://html5up.net">HTML5 UP</Link></li>
+                        <li>&copy; ICTAK</li><li>Design: <Link to="/">BATCH 1 - Group 6</Link></li>
                     </ul>
                 </footer>
 
